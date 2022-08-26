80342c5085504979430944c61844e213bea301b0
Ritika-Pandey01 <ritika.pandey2020@vitbhopal.ac.in>
2022-08-26 23:55:33 +0530

    Started setting up Navbar

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Navbar from "./navbar/Navbar";
 function App() {
   return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
+    <>
+      <Navbar/>
+    </>
   );
 }
 
