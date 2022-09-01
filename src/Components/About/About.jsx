fa35a1f98a8ae82b529635f09fdfa79f4bffbbf8
Ritika-Pandey01 <ritika.pandey2020@vitbhopal.ac.in>
2022-09-01 23:43:27 +0530

    Created About me page

---
+++ src/Components/About/About.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./about.css";
+
+function About() {
+  return (
+    <>
+      <div className="about">
+      <h1>ABOUT ME</h1>
+      <p>Hello, my name is Ritika.</p>
+      </div>
+    </>
+  )
+}
+
+export default About
--- src/Components/App.js
+++ src/Components/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "./navbar/Navbar";
 import Home from "./Home/Home";
+import About from "./About/About";
 function App() {
   return (
     <>
       <Navbar/>
       <Home/>
+      <About/>
     </>
   );
 }
