eecca34a3f28b0a96b1fe58ba91a2e619fe671c5
Ritika-Pandey01 <ritika.pandey2020@vitbhopal.ac.in>
2022-09-01 23:59:50 +0530

    Added content to about

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -5,8 +5,12 @@ function About() {
   return (
     <>
       <div className="about">
+      <div className="textSection">
       <h1>ABOUT ME</h1>
-      <p>Hello, my name is Ritika.</p>
+      <p>Hello, my name is Ritika Pandey.I learn new technologies and am deeply interested in web development. My interest in web development started in 2021, when I started with a uni project for credits. It brought me so much more than just project.</p>
+      <p>I have had the privilege to work as a Cloud Security Solutions Engineer Intern at Pingsafe and I am currently a Frontend Developer Intern in the same organization.</p>
+      <p>I also enjoy working in communities and am very  active on tech twitter. I am a part of WiCYs, EddieHub and have also been an active volunteer at Null:the open source community.Apart from this I have been a speaker in one of the talk series by Infosec Girls.</p>
+      </div>
       </div>
     </>
   )
