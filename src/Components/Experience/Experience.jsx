a4f1638d1de1952c13fef071ebc5144d0f00bfe3
Ritika-Pandey01 <ritika.pandey2020@vitbhopal.ac.in>
2022-09-06 23:34:31 +0530

    Added basic styling and content of experience

--- src/Components/Experience/Experience.jsx
+++ src/Components/Experience/Experience.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from 'react';
+import './experience.css';
 
 function Experience() {
   return (
     <>
-      <h1>Experiences:</h1>
+    <h1>Experiences :</h1>
+    <div className='experience'>
+
+    
+      
+      <div className='Intern1'>
+      <h2>Cloud Security Solutions Engineer Intern</h2>
+      <ul>
+        <li>Researched and documented various security vulnerabilities in
+the AWS and Azure cloud infrastructure.</li>
+<li>Tested existing plugins made using Node.js and reported edge
+cases to avoid failures.</li>
+<li>Researched, identified and removed various cloud misconfigurations.</li>
+      </ul>
+      </div>
+      <div className='Intern2'>
+      <h2>Frontend Developer Intern</h2>
+      <p>Working on various socials, creating HTML Emails for reporting various vulnerabilities and client side communication.</p>
+      </div>
+      <div>
+
+      </div>
+      </div>
     </>
   )
 }
