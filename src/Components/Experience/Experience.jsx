a0950d2969f42886be53afcaad5549c2fb296b38
Ritika-Pandey01 <ritika.pandey2020@vitbhopal.ac.in>
2022-09-07 23:50:49 +0530

    Refactored Experience Section

--- src/Components/Experience/Experience.jsx
+++ src/Components/Experience/Experience.jsx
@@ -3,31 +3,30 @@ import './experience.css';
 
 function Experience() {
   return (
-    <>
-    <h1>Experiences :</h1>
-    <div className='experience'>
+    <div className='bodyExp'>
+      <h1>EXPERIENCE </h1>
+      <div className='experience'>
+        <ul>
+          <li>
+            <h3 className='title'>Cloud Security Solutions Engineer Intern</h3>
+            <p>Researched and documented various security vulnerabilities in
+              the AWS and Azure cloud infrastructure.
+              Tested existing plugins made using Node.js and reported edge
+              cases to avoid failures.
+              Researched, identified and removed various cloud misconfigurations.</p>
+              <span className='circle'></span>
+              <span className='date'>October,2021 - March,2022 </span>
+          </li>
+          <li>
+            <h3 className='title'>Frontend Developer Intern</h3>
+            <p>Working on various communication channels for delivering user oriented responses for our product. Working with HTML Email as well.</p>
+              <span className='circle'></span>
+              <span className='date'>August,2022 - Present </span>
+          </li>
+        </ul>
+      </div>
 
-    
-      
-      <div className='Intern1'>
-      <h2>Cloud Security Solutions Engineer Intern</h2>
-      <ul>
-        <li>Researched and documented various security vulnerabilities in
-the AWS and Azure cloud infrastructure.</li>
-<li>Tested existing plugins made using Node.js and reported edge
-cases to avoid failures.</li>
-<li>Researched, identified and removed various cloud misconfigurations.</li>
-      </ul>
-      </div>
-      <div className='Intern2'>
-      <h2>Frontend Developer Intern</h2>
-      <p>Working on various socials, creating HTML Emails for reporting various vulnerabilities and client side communication.</p>
-      </div>
-      <div>
-
-      </div>
-      </div>
-    </>
+    </div>
   )
 }
 
